f10efff841806b18a7c08371c2efe3914be0d133
YashMunjal <yashmu45@gmail.com>
2020-05-26 21:50:15 +0530

    interrupts

--- interruptstubs.s
+++ interruptstubs.s
@@ -1,3 +1,31 @@
 .section .text
 
-.extern
+.extern _ZN4myos21hardwarecommunication16InterruptManager15HandleInterruptEhj
+
+.macro HandleInterruptRequest num
+.global _ZN4myos21hardwarecommunication16InterruptManager26HandleInterruptRequest\num\()Ev
+_ZN4myos21hardwarecommunication16InterruptManager26HandleInterruptRequest\num\()Ev: 
+
+int_bottom:
+
+    pusha
+    pushl %ds
+    pushl %es
+    pushl %fs
+    pushl %gs
+    
+    pushl %esp
+    push(interruptnumber)
+    call _ZN4myos21hardwarecommunication16InterruptManager15HandleInterruptEhj
+    movl %eax, %esp 
+
+    popl %gs
+    popl %fs
+    popl %es
+    popl %ds
+    pusha
+
+    iret
+    
+.data 
+    interruptnumber: .byte 0;
